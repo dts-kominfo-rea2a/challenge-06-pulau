1436e6d3cf326a20bd24096f8742bc978070bde3
pulau <muchammad.anas@gmail.com>
2022-06-12 19:01:53 +0700

    10 - JS Callback Part 2 - Tantangan

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require("fs");
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,7 +19,31 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = (fnCallback) => {
+  let data = [];
+  fs.readFile(file1, (err, data1) => {
+    if (err) {
+      console.log(err);
+    } else {
+      data.push(JSON.parse(data1).message.split(" ")[1]);
+      fs.readFile(file2, (err, data2) => {
+        if (err) {
+          console.log(err);
+        } else {
+          data.push(JSON.parse(data2)[0].message.split(" ")[1]);
+          fs.readFile(file3, (err, data3) => {
+            if (err) {
+              console.log(err);
+            } else {
+              data.push(JSON.parse(data3)[0].data.message.split(" ")[1]);
+              fnCallback(data);
+            }
+          });
+        }
+      });
+    }
+  });
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
